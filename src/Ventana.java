52701a4c6045e23474ac90380f7fa0bddda3a391
Bonnielamb6 <edgartrabajos2205@gmail.com>
2024-03-12 15:18:37 -0600

    Numeros y Fecha
    Implementacion de los numeros en el reloj y mostrar la fecha debajo

--- src/Ventana.java
+++ src/Ventana.java
@@ -4,7 +4,11 @@ import java.awt.image.BufferedImage;
 import java.time.LocalDateTime;
 import java.time.temporal.TemporalField;
 import java.util.Calendar;
-
+/*
+*TODO
+* implementar la fecha en un jlabel
+* implementar los numeros del reloj
+* */
 public class Ventana extends JPanel implements Runnable {
     private final BufferedImage puerroSegundos;
     private final BufferedImage puerroMinutos;
@@ -20,18 +24,56 @@ public class Ventana extends JPanel implements Runnable {
     private int millis = -10;
     private int prevMinChange = -10;
     private int prevHourChange = -5;
-
+    private int dia;
+    private int mes;
+    private int anio;
 
     public Ventana() {
         Dimension dimension = new Dimension(1000, 1000);
         setPreferredSize(dimension);
+        Calendar calendar = Calendar.getInstance();
 
+        dia = calendar.get(Calendar.DAY_OF_MONTH);
+        mes = calendar.get(Calendar.MONTH);
+        anio = calendar.get(Calendar.YEAR);
         //Fondo del reloj
+
         fondo = new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_ARGB);
-        Graphics gfondo = fondo.getGraphics();
+        Graphics2D gfondo = (Graphics2D) fondo.getGraphics();
+        gfondo.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         ImageIcon iconoRelojFondo = new ImageIcon(getClass().getResource("img/clock_hatsune_miku.png"));
         Image img = iconoRelojFondo.getImage();
         gfondo.drawImage(img, 0, -50, 1000, 1000, null);
+        Font fuente = new Font(gfondo.getFont().getName(), Font.PLAIN, 50);
+        gfondo.setFont(fuente);
+        gfondo.drawString("12",465,130);
+        gfondo.drawString("3",490+320,130+330);
+        gfondo.drawString("6",480,800);
+        gfondo.drawString("9",490-330,130+330);
+        gfondo.setStroke(new BasicStroke(10));
+        //TODO dibujar las lineas en cada hora correctamente
+        //linea de la 1
+        //gfondo.drawLine(620,180,650,140);
+        gfondo.drawString("1",640,175);
+        //linea de las 2
+        gfondo.drawString("2",765,285);
+        //linea de las 4
+        gfondo.drawString("4",760,640);
+        //linea de las 5
+        gfondo.drawString("5",640,745);
+        //linea de las 7
+        gfondo.drawString("7",320,735);
+        //linea de las 8
+        gfondo.drawString("8",200,625);
+        //linea de las 10
+        gfondo.drawString("10",185,290);
+        //linea de las 11
+        gfondo.drawString("11",295,175);
+        gfondo.setColor(Color.BLACK);
+        gfondo.fillRect(400,580,200,50);
+        gfondo.setColor(Color.WHITE);
+        gfondo.setFont(fuente = new Font(gfondo.getFont().getName(), Font.PLAIN, 30));
+        gfondo.drawString(""+dia+"/"+mes+"/"+anio,435,615);
         gfondo.dispose();
 
         //Minutero del reloj
